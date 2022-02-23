d797a0b3bd563c6708205cc852af3450d744181e
ManuMedeiros <manu.medeiros94@gmail.com>
2022-02-22 21:26:11 -0300

    exercicio estrutura de dados

---
+++ estrutura.js
@@ -0,0 +1,41 @@
+const Soul_society = [
+    {
+        capitao: true,
+        nome: "Byakuya Kuchiki",
+        esquadrao: 06,
+        tenente: true,
+        zanpakuto: {
+            shikai: "Senbonzakura",
+            bankai: "Senbonzakura Kageyoshi",
+        },
+        insignia: "Camélia",
+        Cor: "Azul Escuro",
+    },
+    {
+        capitao: true,
+        nome: "Rukia Kuchiki",
+        esquadrao: 13,
+        tenente: false,
+        zanpakuto: {
+            shikai: "Sode no Shirayuki",
+            bankai: "Hakka no Togame",
+        },
+        insignia: "Galanthus",
+        Cor: "Bordô",
+    },
+    {
+        capitao: true,
+        nome: "Zaraki Kenpachi",
+        esquadrao: 11,
+        tenente: true,
+        zanpakuto: {
+            shikai: "Nozarashi",
+            bankai: "Sem nome",
+        },
+        insignia: "Milefólio",
+        Cor: "Lavanda",
+    },
+
+];
+
+console.log(Soul_society, 'Soul_society')
