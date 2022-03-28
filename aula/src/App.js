9529ff9fabc5813b6bff0ab6307d3ed96f2f0d42
ManuMedeiros <manu.medeiros94@gmail.com>
2022-03-28 18:30:31 -0300

    aula funcao

--- aula/src/App.js
+++ aula/src/App.js
@@ -1,31 +1,15 @@
 import "./App.css";
-import Card from "./Components/Card";
-import Links from "./Components/Links";
-import { textCard } from "./text";
+import Button from "./Components/Button";
+import Input from "./Components/Input";
+import { CallAlert, TypeText } from "./Utils";
 
 function App() {
 
-  const showLink = true
-
   return (
     <div className="App">
-      <div>
-        <Card title={"Livro de Matheus"} text={textCard.mateus} />
-        <Card title={"Livro de Marcos"} text={textCard.marcos} />
-        <Card title={"Livro de Lucas"} text={textCard.lucas} />
-        <Card title={"Livro de João"} text={textCard.joao} />
-      </div>
-      <div className="links">
-        <Links
-          link={"https://www.youtube.com/c/%C3%81guaVivaRibeir%C3%A3oPires"}
-          church={"Igreja Batista Água Viva"}
-          showLink={showLink}
-        />
-        <Links
-          link={"https://www.youtube.com/c/IgrejaEvAVideira"}
-          church={"Igreja Batista Videira"}
-          showLink={showLink}
-        />
+      <div className="containder">
+        <Input onChange={(e) => { TypeText(e.target.value) }} />
+        <Button call={() => CallAlert("Maça", "Cenoura")}/>
       </div>
     </div>
   );
---
+++ aula/src/Utils/index.jsx
@@ -0,0 +1,7 @@
+export const TypeText = (typing) => {
+    console.log(`${typing} É muito louco!`)
+}
+
+export const CallAlert = (fruit, juicy) => {
+    alert(`Profiro a fruta ${fruit} do que o suco de ${juicy}`)
+}
