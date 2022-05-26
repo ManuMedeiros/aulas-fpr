a9dc1ae874da5f290c86db02d1b0b03ea8708cfd
ManuMedeiros <manu.medeiros94@gmail.com>
2022-05-25 23:37:14 -0300

    metodo get

---
+++ aula/src/Components/Text/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Text = ({text}) => {
+    return <span>{text}</span>
+}
+
+export default Text;
--- aula/src/Pages/Home/index.jsx
+++ aula/src/Pages/Home/index.jsx
@@ -1,18 +1,57 @@
-import React from "react";
-import Links from "../../Components/Links";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Image from "../../Components/Image";
+import Title from "../../Components/title";
+import Text from "../../Components/Text";
+import "./style.css";
 
 const Home = () => {
+
+    const [pokedex, setPokedex] = useState();
+
+    useEffect(() => {
+        axios.get('https://aulas-fpr-default-rtdb.firebaseio.com/pokemon.json')
+            .then((response) => {
+                console.log(response.data, 'response')
+                setPokedex(response?.data)
+            })
+    }, [])
+
     return (
-        <>
-            <Links
-                text={'Split - animes'}
-                link={'/Animes?animes=naruto&categoria=ninja&principal=naruto'}
-            />
-            <Links
-                text={'Query String - Avatares'}
-                link={'/Avatares?avatar=kyoshi&elemento=terra&nacao=terra&inimigo=chin&animal=desconhecido'}
-            />
-        </>
+        <div className="container-home">
+            <div>
+            <h2>Fist Evoulution</h2>
+                {Object.values(pokedex.firstEvolution).map(first => {
+                    console.log(first, 'first')
+                    return (
+                        <>
+                            <div className="card-pokemon">
+                                <Image src={first?.img} />
+                                <Title text={`Name: ${first?.name}`} />
+                                <Text text={`Number: ${first?.numer}`} />
+                                <Text text={`Type: ${first?.type}`} />
+                            </div>
+                        </>
+                    )
+                })}
+            </div>
+            <div>
+            <h2>Second Evoulution</h2>
+                {Object.values(pokedex.secondEvolution).map(second => {
+                    console.log(second, 'second')
+                    return (
+                        <>
+                            <div className="card-pokemon">
+                                <Image src={second?.img} />
+                                <Title text={`Name: ${second?.name}`} />
+                                <Text text={`Number: ${second?.numer}`} />
+                                <Text text={`Type: ${second?.type}`} />
+                            </div>
+                        </>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
